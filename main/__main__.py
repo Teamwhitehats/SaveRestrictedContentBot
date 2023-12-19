7f14138661c26fc932c360598220694b2c845a75
Teamwhitehats <148607261+Teamwhitehats@users.noreply.github.com>
2023-12-19 12:19:53 +0530

    Update __main__.py

--- main/__main__.py
+++ main/__main__.py
@@ -17,7 +17,7 @@ for name in files:
 
 #Don't be a thief 
 print("Successfully deployed!")
-print("By MaheshChauhan • Team_whitehat")
+print("By Hackervipking • Team_whitehat")
 
 if __name__ == "__main__":
     bot.run_until_disconnected()
